b699eacc8ba02f43cfb4a40b88434b043fe4c814
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-04-23 22:44:45 +0530

    update commit

--- src/App.jsx
+++ src/App.jsx
@@ -48,12 +48,6 @@ function App() {
                   <StudentRegistrationForm />
                 </div>
               } />
-              {/* <Route path="*" element={
-                <div className="text-center py-20">
-                  <h1 className="text-4xl font-bold text-gray-800">404 - Page Not Found</h1>
-                  <p className="mt-4 text-lg text-gray-600">The page you're looking for doesn't exist.</p>
-                </div>
-              } /> */}
             </Routes>
         </div>
         </div>
@@ -64,3 +58,7 @@ function App() {
 }
 
 export default App
+
+
+
+
